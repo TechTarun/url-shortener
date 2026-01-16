aed3a13c7d4212ceef1a612067b4533838c6320f
Tarun Agarwal <tarunagarwal27.99@gmail.com>
2026-01-16 11:34:59 +0530

    added concurrent testing for snowflake id

--- pkg/idgen/snowflake_generator_test.go
+++ pkg/idgen/snowflake_generator_test.go
@@ -1,6 +1,7 @@
 package idgen
 
 import (
+	"sync"
 	"testing"
 )
 
@@ -21,5 +22,56 @@ func TestSnowflakeGenerator_IsUnique(t *testing.T) {
 			t.Fatalf("Duplicate found at i = %d", i)
 		}
 		seen[short_code] = true
+	}
+}
+
+func TestSnowflake_ConcurrentUniqueness(t *testing.T) {
+	const (
+		goroutines    = 50
+		idsPerRoutine = 20000
+		expectedTotal = goroutines * idsPerRoutine
+	)
+
+	gen, err := NewSnowflakeGenerator(8, 1)
+	if err != nil {
+		t.Fatalf("failed to create snowflake: %v", err)
+	}
+
+	var (
+		wg      sync.WaitGroup
+		seen    sync.Map
+		longUrl string
+	)
+
+	longUrl = "https://www.google.com"
+
+	wg.Add(goroutines)
+
+	for i := 0; i < goroutines; i++ {
+		go func() {
+			defer wg.Done()
+
+			for j := 0; j < idsPerRoutine; j++ {
+				id, _ := gen.GenerateShortCode(longUrl)
+
+				// Check for duplicates
+				if _, exists := seen.LoadOrStore(id, true); exists {
+					t.Fatalf("duplicate ID detected: %s", id)
+				}
+			}
+		}()
+	}
+
+	wg.Wait()
+
+	// Count results
+	count := 0
+	seen.Range(func(_, _ any) bool {
+		count++
+		return true
+	})
+
+	if count != expectedTotal {
+		t.Fatalf("expected %d ids, got %d", expectedTotal, count)
 	}
 }
