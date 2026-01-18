298402a5bcd3d9a7508621a4de6167b92046918d
Tarun Agarwal <tarunagarwal27.99@gmail.com>
2026-01-18 22:28:11 +0530

    infra: managed env variable config

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,15 +3,29 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
+	"strconv"
+	"time"
 	"url-shortener/internal/shortener"
 	"url-shortener/internal/shortener/storage"
 	"url-shortener/pkg/idgen"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	// repo := storage.NewInMemoryStore()
-	repo := storage.NewRedisStore(0)
-	generator, _ := idgen.NewSnowflakeGenerator(8, 1)
+	_ = godotenv.Load()
+	ttlStr := os.Getenv("REDIS_TTL")
+	ttl, err := time.ParseDuration(ttlStr)
+	if err != nil {
+		ttl = 24 * time.Hour
+	}
+	shortCodeLength, _ := strconv.Atoi(os.Getenv("SHORT_CODE_LENGTH"))
+	nodeId, _ := strconv.ParseInt(os.Getenv("NODE_ID"), 10, 64)
+
+	repo := storage.NewRedisStore(ttl)
+	generator, _ := idgen.NewSnowflakeGenerator(shortCodeLength, nodeId)
 	service := shortener.NewService(repo, generator)
 	handler := shortener.NewHandler(service)
 
